d0d2abdac638c4f9a154ff13924714af14ca9bcb
taiki-kuraishi <131664944+taiki-kuraishi@users.noreply.github.com>
2023-11-30 22:43:32 +0900

    cerate NuxtPages

---
+++ app/pages/login.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>this is the login page </h1>
+        <button class="google-button" @click="googleSignUp"><span>Googleで登録</span></button>
+    </div>
+</template>
+<script setup lang="ts">
+import { useAuth } from '../composables/useAuth';
+
+const googleSignUp = () => {
+    const { googleSignUp } = useAuth()
+    googleSignUp()
+}
+</script>
