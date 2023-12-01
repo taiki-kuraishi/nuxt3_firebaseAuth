3a11db20a6e5d6e7bc5b3d406b08c9b19120c5f6
taiki-kuraishi <131664944+taiki-kuraishi@users.noreply.github.com>
2023-12-01 21:41:17 +0900

    Update content.vue

--- app/pages/content.vue
+++ app/pages/content.vue
@@ -1,5 +1,18 @@
 <template>
     <div>
-        this is the content page
+        <h1>this is the content page</h1>
+        <p>{{ user }}</p>
+        <button @click="signOut">signOut</button>
     </div>
 </template>
+<script setup lang="ts">
+import { useAuth } from "~/composables/auth";
+import { useUser } from "~/composables/user";
+
+const { user } = useUser();
+
+const signOut = async (): Promise<void> => {
+    await useAuth().signOut();
+    await navigateTo('/signIn')
+}
+</script>
