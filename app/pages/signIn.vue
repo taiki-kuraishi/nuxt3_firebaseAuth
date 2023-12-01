6d2f6d4f5d798827a0d01661414bf0106c8e7258
taiki-kuraishi <131664944+taiki-kuraishi@users.noreply.github.com>
2023-12-01 21:41:21 +0900

    Create signIn.vue

---
+++ app/pages/signIn.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>this is the signIn page </h1>
+        <button @click="signIn()">signIn</button>
+    </div>
+</template>
+<script setup lang="ts">
+import { useAuth } from '~/composables/auth';
+
+const signIn = async (): Promise<void> => {
+    await useAuth().signIn();
+    navigateTo('/content')
+}
+</script>
