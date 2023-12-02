a275cc6ccb35cacfc2b39e9b52b64d9ac10e420e
taiki-kuraishi <131664944+taiki-kuraishi@users.noreply.github.com>
2023-12-02 17:13:44 +0900

    firebase init

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -2,5 +2,10 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   plugins: [
     'plugins/firebase.client.ts'
-  ]
+  ],
+  nitro: {
+    firebase: {
+      gen: 2
+    }
+  }
 })
