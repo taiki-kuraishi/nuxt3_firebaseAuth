27655c5bd1b4248f2b1f9310c5f980cca23b0050
taiki-kuraishi <131664944+taiki-kuraishi@users.noreply.github.com>
2023-12-03 12:14:50 +0900

    Update signIn.vue

--- app/pages/signIn.vue
+++ app/pages/signIn.vue
@@ -5,7 +5,7 @@
     </div>
 </template>
 <script setup lang="ts">
-import { useAuth } from '~/composables/auth';
+import { useAuth } from '../composables/auth';
 
 const signIn = async (): Promise<void> => {
     await useAuth().signIn();
