d39508cb0aa319209ff4a2004f81e7e381ab96f2
taiki-kuraishi <131664944+taiki-kuraishi@users.noreply.github.com>
2023-12-03 12:14:54 +0900

    Update nuxt.config.ts

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -6,12 +6,12 @@ export default defineNuxtConfig({
   ssr: false,
   runtimeConfig: {
     public: {
-      apiKey: "AIzaSyCRSR9taHw2VeE2Ft27o5LtS_twKx1D0WI",
-      authDomain: "nuxt3-ec8df.firebaseapp.com",
-      projectId: "nuxt3-ec8df",
-      storageBucket: "nuxt3-ec8df.appspot.com",
-      messagingSenderId: "840302904340",
-      appId: "1:840302904340:web:fb3dd9ff99f159350d1301"
+      apiKey: process.env.NUXT_FIREBASE_API_KRY,
+      authDomain: process.env.NUXT_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.NUXT_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.NUXT_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.NUXT_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.NUXT_FIREBASE_APP_ID
     }
   }
 })
